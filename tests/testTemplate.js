1d96ef66f1e7727bd9715e9b99ece882e695e43b
humleflue <humleflue@yahoo.dk>
2020-03-13 08:49:20 +0100

    fixed syntax errors in testTemplate.js

--- tests/testTemplate.js
+++ tests/testTemplate.js
@@ -1,14 +1,14 @@
-/*Dette dokument er en template for hvordan vi laver test.*/
+/* Dette dokument er en template for hvordan vi laver test. */
 
-/*npm i --save-dev tape*/
-/*npm i --save-dev tape-promise*/
+/* npm i --save-dev tape */
+/* npm i --save-dev tape-promise */
 
-/*Templates*********************************************************/
-/*Non-Async Functions***********************************************/
-const basicTest = require("tape");
+/* Templates ******************************************************** */
+/* Non-Async Functions ********************************************** */
+const basicTest = require(`tape`);
 
 basicTest(`Skriv kort hvad der testes`, (assert) => {
-  const expected = `forventet ud fra testværdi`; 
+  const expected = `forventet ud fra testværdi`;
   const actual = testFunktion(`testværdi`);
 
   assert.equal(actual, expected,
@@ -17,61 +17,60 @@ basicTest(`Skriv kort hvad der testes`, (assert) => {
   assert.end();
 });
 
-/*Promise Functions*****************************************************/
-const tape = require("tape");
-const _test = require("tape-promise").default;
-const promiseTest = _test(tape);
+/* Promise Functions **************************************************** */
+const tape = require(`tape`);
+const testDecorater = require(`tape-promise`).default;
+
+const promiseTest = testDecorater(tape);
 
 promiseTest(`testPromise skal konvertere string input til numbers`, (assert) => {
-    const expected = `forventet ud fra testværdi`;;
-    
-    return testPromise(`testværdi`).then((actual) => assert.equal(actual, expected,
-        `Giv her en mere detaljeret beskrivelse af hvad der testes`)
-    );
+  const expected = `forventet ud fra testværdi`;
+
+  return testPromise(`testværdi`).then((actual) => assert.equal(actual, expected,
+    `Giv her en mere detaljeret beskrivelse af hvad der testes`));
 });
 
 
-/*React Rendering*****************************************************/
-//Se kilder i bunden. Mangler Template
+/* React Rendering**************************************************** */
+// Se kilder i bunden. Mangler Template
 
-/*Eksempler (der fejler)**********************************************/
-/*Funktionerne der skal testes****************************************/
-function testFunktion(inputValue){
-    return Number(inputValue)+5;
+/* Eksempler (der fejler)********************************************* */
+/* Funktionerne der skal testes*************************************** */
+function testFunktion(inputValue) {
+  return Number(inputValue) + 5;
 }
-function testPromise (inputValue) {
-    return new Promise(function (resolve, reject) {
-      setTimeout(function () {
-        resolve(Number(inputValue) + 5);
-      }, 500);
-    });
-  }
+function testPromise(inputValue) {
+  return new Promise(((resolve) => {
+    setTimeout(() => {
+      resolve(Number(inputValue) + 5);
+    }, 500);
+  }));
+}
 
-/*Non-Async************************************************************/
+/* Non-Async*********************************************************** */
 basicTest(`testFunktion skal konvertere string input til numbers`, (assert) => {
-    const expected = 6;
-    const actual = testFunktion("1");
-  
-    assert.equal(actual, expected,
-        `Funktionen skal give ${expected} og giver ${actual}\n` +
-        `Giv her en mere detaljeret beskrivelse af hvad der testes, så der hurtigere kan findes fejlene\n` +
-        `Desuden må man meget gerne gøre beskrivelserne længere hvis man har behov for det`);
-  
-    assert.end();
-  });
+  const expected = 6;
+  const actual = testFunktion(`1`);
 
-/*Promise*****************************************************************/
-promiseTest(`testPromise skal konvertere string input til numbers`, (assert) => {
-    const expected = 6;
-    
-    return testPromise("1").then((actual) => assert.equal(actual, expected,
-            `Funktionen skal give ${expected} og giver ${actual}\n` +
-            `Giv her en mere detaljeret beskrivelse af hvad der testes, så der hurtigere kan findes fejlene\n` +
-            `Desuden må man meget gerne gøre beskrivelserne længere hvis man har behov for det`)
-    );
+  assert.equal(actual, expected,
+    `Funktionen skal give ${expected} og giver ${actual}\n`
+        + `Giv her en mere detaljeret beskrivelse af hvad der testes, så der hurtigere kan findes fejlene\n`
+        + `Desuden må man meget gerne gøre beskrivelserne længere hvis man har behov for det`);
+
+  assert.end();
 });
 
-/*Kilder*****************************************************************/
-//Tape:              https://medium.com/javascript-scene/why-i-use-tape-instead-of-mocha-so-should-you-6aa105d8eaf4
-//Tape-Promise:      https://github.com/jprichardson/tape-promise 
-//Shallow Rendering: https://simonsmith.io/unit-testing-react-components-without-a-dom
+/* Promise**************************************************************** */
+promiseTest(`testPromise skal konvertere string input til numbers`, (assert) => {
+  const expected = 6;
+
+  return testPromise(`1`).then((actual) => assert.equal(actual, expected,
+    `Funktionen skal give ${expected} og giver ${actual}\n`
+            + `Giv her en mere detaljeret beskrivelse af hvad der testes, så der hurtigere kan findes fejlene\n`
+            + `Desuden må man meget gerne gøre beskrivelserne længere hvis man har behov for det`));
+});
+
+/* Kilder**************************************************************** */
+// Tape:              https://medium.com/javascript-scene/why-i-use-tape-instead-of-mocha-so-should-you-6aa105d8eaf4
+// Tape-Promise:      https://github.com/jprichardson/tape-promise
+// Shallow Rendering: https://simonsmith.io/unit-testing-react-components-without-a-dom
