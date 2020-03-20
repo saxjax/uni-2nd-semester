f9a630462fe21c5ff5f804ac3c9b0c2d15cb594e
Lasse Skaalum <lskaal19@student.aau.dk>
2020-03-20 11:14:43 +0100

    Cleanup and polishing

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const { Server } = require(`./node/Server/Server.js`);
 
 const Dev = new Server();
-Dev.startServer(`Developmentserver up and running på port ${Dev.port}`);
+Dev.startServer(`Developmentserver up and running on localhost:${Dev.port}`);
 Dev.query();
