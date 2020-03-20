076336f415db8f08d219ac7a65d8d70e73d5b058
Lasse Skaalum <lskaal19@student.aau.dk>
2020-03-20 15:16:52 +0100

    Fixed npms

---
+++ npm-everything.bat
@@ -0,0 +1,23 @@
+echo Installing EXPRESS
+call npm install express --save
+echo Installing MYSQL 
+call npm install mysql --save
+echo Installing BODY-PARSER
+call npm install body-parser --save
+echo Installing PATH 
+call npm install path --save
+echo Installing EJS 
+call npm install ejs --save
+
+echo Installing TAPE 
+call npm install tape --save-dev
+echo Installing TAPE-PROMISE
+call npm install tape-promise --save-dev
+echo Installing TAP-SPEC
+call npm install tap-spec --save-dev
+echo Installing ESLINT
+call npm install eslint --save-dev
+echo Installing JSDOM
+call npm install jsdom --save-dev
+
+pause
