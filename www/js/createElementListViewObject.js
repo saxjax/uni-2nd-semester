3c7f38c7ca4a25761a77412586141cf83ab4eb31
Jakob Skov Søndergård <saxjax@saxjax.dk>
2020-03-21 01:04:03 +0100

    cards bliver opdateret med text alt efter deres type, Section, Flashcard,Quizz

--- www/js/createElementListViewObject.js
+++ www/js/createElementListViewObject.js
@@ -15,7 +15,7 @@
 // let deck = new Array()
 
 
-var titles = ["Key word 1", "Key word 2", "Key word 3", "Key word 4", "Key word 5", "Key word 6", "Key word 7", "Key word 8", "Key word 9", "Key word 10", "Key word J", "Key word Q", "Key word K"];
+var titles = ["Key word 1", "Key word 2", "Key word 3", "Key word 4", "Key word 5", "Key word 6", "Key word 7", "Key word 8", "Key word 9", "Key word 10", "Key word 11", "Key word 12", "Key word 13"];
 var content = ["Content : Key word 1 Ave verum corpus,est in ad hoc triagtig fyldingen snustrok", "Content : Key word 2 Ave verum corpus,est in ad hoc triagtig fyldingen snustrok", "Content : Key word 3 Ave verum corpus,est in ad hoc triagtig fyldingen snustrok", "Content : Key word 4 Ave verum corpus,est in ad hoc triagtig fyldingen snustrok", "Content : Key word 5 Ave verum corpus,est in ad hoc triagtig fyldingen snustrok", "Content : Key word 6 Ave verum corpus,est in ad hoc triagtig fyldingen snustrok", "Content : Key word 7 Ave verum corpus,est in ad hoc triagtig fyldingen snustrok", "Content : Key word 8 Ave verum corpus,est in ad hoc triagtig fyldingen snustrok", "Content : Key word 9 Ave verum corpus,est in ad hoc triagtig fyldingen snustrok", "Content : Key word 10 Ave verum corpus,est in ad hoc triagtig fyldingen snustrok", "Content : Key word 11 Ave verum corpus,est in ad hoc triagtig fyldingen snustrok", "Content : Key word 12 Ave verum corpus,est in ad hoc triagtig fyldingen snustrok", "Content : Key word 13 Ave verum corpus,est in ad hoc triagtig fyldingen snustrok"];
 var elementTypes = ["Section", "Quizz", "Flashcard"];
 var deck = new Array();
@@ -28,8 +28,27 @@ function getDeck()
 	{
 		for(var x = 0; x < titles.length; x++)
 		{
-			var card = {Value: titles[x], Content: content[x], ElementType: elementTypes[i]};
+			var card = {Value: titles[x], Content: content[x], ElementType: elementTypes[i], SectionID: x};
 			deck.push(card);
+			
+
+			switch (card.ElementType) {
+				case "Section": 
+				card.Content = readText(card.SectionID);
+				break;
+
+				case "Quizz"  : 
+				card.Value = "What is " + getKeywordsFrom(card.SectionID);
+				break;
+
+				case "Flashcard": 
+				card.Value ="Define " + getKeywordsFrom(card.SectionID);
+				break;
+
+				default :  break;
+			} 
+				
+			
 		}
 	}
 
@@ -52,6 +71,49 @@ function shuffle()
 
 	renderDeck();
 }
+
+
+function readText(path){
+	return  `Section #${path}----- P2-vidensdeling
+
+	Kommando til at starte programmet
+	node main.js
+	
+	Kommando til at gennemgå alle test:
+	node_modules\.bin\tape tests/**/test.*.js | node_modules\.bin\tap-spec
+	
+	Kommando til at arbejde testbaseret i realtime:
+	nodemon tests/backend_eller_frontend/mappeMedDinTest/test.filNavn.js | .\node_modules\.bin\tap-simple
+	
+	eksporter dine funktioner/objekter i bunden af filen dine tests er knyttet til sådan her:
+	
+	module.exports = {
+	  functionOne,
+	  functionTwo,
+	  objectOne,
+	  objectTwo
+	};
+	
+	For hver eksporteret funktion skal der være en unittest. Eksporteres en Class skal alle metoder unittestes.
+	
+	## Node moduler
+	
+	npm install express --save
+	npm install mysql
+	npm i tape
+	npm i --save-dev tape-promise
+	npm i tap-spec
+	npm install eslint --save-dev
+	npm install ejs
+	
+	`
+}
+
+function getKeywordsFrom(SectionID){
+	return titles[SectionID];
+}
+
+
 
 function renderDeck()
 {
@@ -93,5 +155,10 @@ function load()
 	// shuffle();
 	renderDeck();
 }
+
+
+
+
+
 
 window.onload = load;
