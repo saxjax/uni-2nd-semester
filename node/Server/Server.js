ee3d81fc5822b747c78ba16a3eaf3ad6f276751e
MartinBruun <mbm93fen@gmail.com>
2020-03-22 19:40:11 +0100

    Standardiseret Server og implementeret navbar i views

--- node/Server/Server.js
+++ node/Server/Server.js
@@ -26,14 +26,14 @@ class Server {
 
   urlPatterns() {
     const Show = new ViewController();
-    this.app.get(`/`, (req, res) => Show.homePage(req, res));
-    this.app.get(`/register`, (req, res) => Show.registerPage(req, res));
-    this.app.get(`/login`, (req, res) => Show.loginPage(req, res));
-    this.app.get(`/evalueringer`, (req, res) => Show.evalueringerPage(req, res));
+    this.app.get(`/`,                   (req, res) => Show.homePage(req, res));
+    this.app.get(`/register`,           (req, res) => Show.registerPage(req, res));
+    this.app.get(`/login`,              (req, res) => Show.loginPage(req, res));
+    this.app.get(`/evalueringer`,       (req, res) => Show.evalueringerPage(req, res));
     this.app.get(`/evalueringer/:type`, (req, res) => Show.evalueringerTypePage(req, res));
-    this.app.get(`/rapport`, (req, res) => Show.rapportPage(req, res));
-    this.app.get(`/rapport/:afsnit`, (req, res) => Show.rapportSectionPage(req, res));
-    this.app.get(`/elementList`, (req, res) => Show.elementList(req, res));
+    this.app.get(`/rapport`,            (req, res) => Show.rapportPage(req, res));
+    this.app.get(`/rapport/:afsnit`,    (req, res) => Show.rapportSectionPage(req, res));
+    this.app.get(`/elementList`,        (req, res) => Show.elementList(req, res));
   }
 
   redirectPatterns() {
@@ -45,7 +45,7 @@ class Server {
   staticMiddleware() {
     this.app.use(`/js`,       express.static(`${this.root}/www/js`));
     this.app.use(`/css`,      express.static(`${this.root}/www/css`));
-    this.app.use(`/img`, express.static(`${this.root}/www/img`));
+    this.app.use(`/img`,      express.static(`${this.root}/www/img`));
     this.app.use(`/icon.ico`, express.static(`${this.root}/www/img/icon.ico`));
   
   }
