24ad3f33ac56eb4fe57cee7ff94d5194adaabeca
Dreadnogh <mike_johansen101@yahoo.com>
2020-04-20 15:55:04 +0200

    Register is now working

--- node/Models/User.js
+++ node/Models/User.js
@@ -32,7 +32,7 @@ class User extends Model {
    * Output: En row af data fra 1 bruger.
    */
   async loginValid() {
-    return this.query(`SELECT *`, `username = "${this.username}" AND password = "${this.password}"`)
+    return this.query(`SELECT *`, `USER_NAME = "${this.username}" AND PASSWORD = "${this.password}"`)
       .then((result) => result)
       .catch((error) => error);
   }
@@ -48,7 +48,7 @@ class User extends Model {
   async validateRegister() {
     let validationCheck = false;
     if (!isEmpty(this.username) && !isEmpty(this.firstName) && !isEmpty(this.lastName) && !isEmpty(this.email)) {
-      this.data = await this.query(`SELECT *`, `username = "${this.username}" OR email = "${this.email}"`);
+      this.data = await this.query(`SELECT *`, `USER_NAME = "${this.username}" OR EMAIL = "${this.email}"`);
       if (this.data.length > 0) {
         console.log(`User already registered`);
       }
@@ -57,6 +57,30 @@ class User extends Model {
       }
     }
     return validationCheck;
+  }
+
+  /* Formål: At oprette en bruger i databasen hvis validering returnerer 'True'
+   * Input : N/A
+   * Output: True/False
+   */
+  async createUser() {
+    try {
+      await this.query(`INSERT`, `USER_NAME = "${this.username}" `
+                     + `AND PASSWORD = "${this.password}" `
+                     + `AND FIRST_NAME = "${this.firstName}" `
+                     + `AND LAST_NAME = "${this.lastName}" `
+                     + `AND UNIVERSITY = "${this.university}" `
+                     + `AND STUDY_SUBJECT = "${this.studySubject}" `
+                     + `AND EMAIL = "${this.email}" `
+                     + `AND SEMESTER = "${this.semester}"`);
+    }
+    catch (error) {
+      console.log(`User with username: ${this.username} FAILED to be created`);
+      console.log(`${error}`);
+      return false;
+    }
+    console.log(`User created with username: ${this.username}`);
+    return true;
   }
 }
 
