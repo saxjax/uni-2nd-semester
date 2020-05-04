441a950308f993d5d38cdcf98cea2bc3ca291264
JacobToftgaardRasmussen <jtra19@student.aau.dk>
2020-05-04 16:00:53 +0200

    eeeeeeehhhhh

---
+++ www/js/useEvaluation.js
@@ -0,0 +1,11 @@
+/* eslint no-undef: 0 */
+/*
+const allQuestionsDiv = document.getElementById(`allQuestions`);
+let questionCount = 0;
+
+data.questions.forEach((question) => {
+  const questionDiv = appendDomNode(`DIV`, allQuestionsDiv, ``, [{ id: `question${questionCount}` }]);
+  appendDomNode(`P`, questionDiv, `${question.question}`);
+  questionCount++;
+});
+ */
