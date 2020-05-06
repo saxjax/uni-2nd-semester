b19527f8a391f1f8aeb2b41122bda630bc9e6043
Rallefar1103 <rhenri18@student.aau.dk>
2020-05-06 14:24:12 +0200

    calculateNextRepetitionForEvaluation()

--- node/Models/AbstractClasses/spacedRepetition.js
+++ node/Models/AbstractClasses/spacedRepetition.js
@@ -12,8 +12,33 @@ class SpacedRepetition {
 
   }
 
-  calculateNextRepetitionForEvaluation() {
-
+  calculateNextRepetitionForEvaluation(evaluationTask) {
+    let rightWrongRatio = 0;
+    
+    if (evaluationTask.correctness === true) { // er svaret korrekt?
+      if (evaluationTask.rep > 0) { // er evalueringsopgaven taget før?
+        if (evaluationTask.history.wrong > 0) { // er evalueringsopgaven svaret forkert før?
+          const rightWrongRatio = calcRightWrongRatio(evaluationTask.history); // udregn rigtig-forkert ratio
+          if (rightWrongRatio >= 3) { // er ratioen større end lig med tre?
+            this.calculateTimestamp(evaluationTask); // beregn tidsstempel
+          }
+          else { 
+            this.calculateTimestamp(evaluationTask);
+          }
+        }
+        else { // hvis den IKKE er blevet svaret forkert før, beregn da korrekt tidsstempel
+          evaluationTask.history.correctness === true;
+          this.calculateTimestamp(evaluationTask);
+        }
+      } // er evalueringen IKKE blevet taget før, tæl da evalueringen op som være repeteret og beregn tidsstempel
+      else {
+        evaluationTask.rep++;
+        this.calculateTimestamp(evaluationTask);
+      }
+    } // hvis der svares forkert på evalueringen beregnes det korrekte tidsstempel 
+    else {
+      this.calculateTimestamp(evaluationTask);
+    }
   }
 
   calculateTimestamp() {
