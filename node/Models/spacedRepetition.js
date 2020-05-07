f7cb7f6313d8515e7c20ab80bf71b304466fd27d
Jakob Skov Søndergård <saxjax@saxjax.dk>
2020-05-07 15:31:02 +0200

    spacedrepetition 3 funktioner, set nextRepetition date, og selectEvaluationsForRepetition  fra et array af results

--- node/Models/spacedRepetition.js
+++ node/Models/spacedRepetition.js
@@ -5,12 +5,11 @@ const { Model } = require(`./AbstractClasses/Model`);
 
 
 
-
-class SpacedRepetition extends Model {
-  constructor(req) {
-    super(req)
-    // this.nextRepetitionTask = [];
-    this.minTimestamp = 24; // 24 timer
+//extend Model
+class SpacedRepetition  {
+  constructor() {
+    // super(req)
+    this.minTimestamp = 20; // 24 timer
     this.comprehentionRatio = 3; // hvert forkert svar kræver mindst 3 rigtige svar for at blive registreret som forstået
   }
 
@@ -25,19 +24,13 @@ class SpacedRepetition extends Model {
 
   }
 
-  // setNextRepTimestampForEvaluations(evaluationLog){
-  //   let tempLog = evaluationLog
-  //   tempLog.forEach((eval)=>{
-  //     eval.nextRepTimeStamp = this.calculateNextRepetitionTimeStampForEvaluation(eval).nextRepTimeStamp;
-  //   })
-  //   return tempLog
-  // }
-
-  // Input: evaluationTask med følgende properties: recentResult, antal tidligere repetitions, wrong answers count, right answers count //
+//spacedrepetition algoritmen udgøres af funktionerne : calculateNextRepetitionTimeStampForEvaluation() og calculateTimeStamp() 
+  /* Formål: Afgør Hvilket timestamp der skal knyttes til et evaluation result  
+  // Input: et evaluationResult med følgende properties: recentResult (true/false), antal tidligere repetitions, failedAttempts, successAttempts //
   // Output: //
-
+*/
   calculateNextRepetitionTimeStampForEvaluation(evaluationResult) {
-    const tempEvalTask = evaluationResult;//kopierer input parameteren fordi vi vil returnere en muteret version.
+    let newRepTimestamp 
     let rightWrongRatio = 0;
     let setMinTimestamp = true;
    
@@ -45,8 +38,8 @@ class SpacedRepetition extends Model {
     if (evaluationResult.recentResult === true) { // er svaret på spørgsmålet korrekt?
       if (evaluationResult.repetitions > 0) { // er evalueringsopgaven taget før?
         if (evaluationResult.failedAttempts > 0) { // er evalueringsopgaven svaret forkert før?
-          rightWrongRatio = (evaluationResult.failedAttempts / evaluationResult.successAttempts); // udregn rigtig-forkert ratio
-          if (rightWrongRatio >= this.comprehentionRatio) { // er comprehention ratioen større end lig med tre?
+          rightWrongRatio = (evaluationResult.successAttempts / evaluationResult.failedAttempts); // udregn rigtig-forkert ratio
+          if (rightWrongRatio < this.comprehentionRatio) { // er comprehention ratioen større end lig med tre?
             setMinTimestamp = true;
           }
           else { 
@@ -66,17 +59,21 @@ class SpacedRepetition extends Model {
     }
 
     // tempEvalTask.rep++;
-    tempEvalTask.nextRepTimeStamp = this.calculateTimeStamp(setMinTimestamp, evaluationResult.successAttempts); // beregn tidsstempel
+    newRepTimestamp = this.calculateTimeStamp(setMinTimestamp, evaluationResult.successAttempts); // beregn tidsstempel
 
-    return tempEvalTask;
+    return newRepTimestamp;
   }
 
-
+/*
+  * Formål: Beregn timestamp for næste spaced-repetition 
+  * Input : setMinimumTimestamp = true/false, successAttempts=(antal gange spørgsmålet er besvaret korrekt)
+  * Output: Dato
+  */
   calculateTimeStamp(setMinTimestamp = true, successAttempts) {
     let newRepTimeStamp= new Date();
     
-    if (setMinTimestamp === true) {
-      newRepTimeStamp.setHours(newRepTimeStamp.getHours()+ this.minTimestamp);
+    if (setMinTimestamp === true) {//hvis der er svaret forkert eller comprehentionRatio ikke er opfyldt
+      newRepTimeStamp.setHours(newRepTimeStamp.getHours()+ 0);
     }
     else {
       newRepTimeStamp.setHours(newRepTimeStamp.getHours()+ ((successAttempts * successAttempts) * this.minTimestamp)  ) ;
@@ -87,51 +84,94 @@ class SpacedRepetition extends Model {
 
 
   /*
-  * NextRepetitiontask
+  * Formål: opret et array med alle de evaluerings objekter som skal afvikles NU
+  * Input : array af Results
+  * Output: array med der reults som skal afvikles nu.
   */
-  populateNextRepetitionTask(evaluationLog) {
-    evaluationLog.forEach((evaluationTask) => {
-      if (evaluationTask.nextRepTimeStamp <= Date.now()) {
-        this.nextRepetitionTask.push(evaluationTask);
+  selectEvaluationsForRepetition(evaluationLog) {
+    let nextRepetitionTask =[]
+    evaluationLog.forEach((evaluationResult) => {
+      if (evaluationResult.nextRepTimeStamp <= Date.now()) {
+        nextRepetitionTask.push(evaluationResult);
       }
     });
+    return nextRepetitionTask;
   }
 
-
-  EvaluationIsDue() {
-    Date.now();
-  }
 }
 
 module.exports = {
   SpacedRepetition,
 };
 
+
+
+
+
+
+
+
+// TEST
+
 const req = {};
 const spacedR = new SpacedRepetition(req);
 let evalLog = []
 let evalTask = {
-  failedAttempts: 2, successAttempts: 2, recentResult: true, rep: 4, nextRepTimeStamp: 0,
+  failedAttempts: 1, successAttempts:1, recentResult: true, repetitions: 3, 
 };
 
-// evalTask = spacedR.calculateNextRepetitionTimeStampForEvaluation(evalTask)
+
+
+evalTask.nextRepTimeStamp = spacedR.calculateNextRepetitionTimeStampForEvaluation(evalTask)
 // evalLog.push(evalTask)
-// console.log(evalTask);
+console.log(`#1:`+evalTask.nextRepTimeStamp);
+
+
+
+
+for (let index = 0; index < 3; index++) {
+
+evalTask.successAttempts ++
+// evalTask.repetitions++
+evalTask.nextRepTimeStamp = spacedR.calculateNextRepetitionTimeStampForEvaluation(evalTask)
+// console.log(`#2:`+evalTask.nextRepTimeStamp);
+const copy = Object.assign({}, evalTask);
+evalLog.push(copy) 
+// console.log(copy)
+}
+for (let index = 0; index < 3; index++) {
+
+  evalTask.failedAttempts ++
+  evalTask.recentResult = false
+  // evalTask.repetitions++
+  evalTask.nextRepTimeStamp = spacedR.calculateNextRepetitionTimeStampForEvaluation(evalTask)
+  // console.log(`#2:`+evalTask.nextRepTimeStamp);
+  const copy = Object.assign({}, evalTask);
+  evalLog.push(copy) 
+  // console.log(copy)
+  }
+
 
 // evalTask.successAttempts ++
-// evalTask = spacedR.calculateNextRepetitionTimeStampForEvaluation(evalTask)
-// evalLog.push(evalTask)
-// console.log(evalTask);
+// evalTask.nextRepTimeStamp = spacedR.calculateNextRepetitionTimeStampForEvaluation(evalTask)
+// console.log(`#3:`+evalTask.nextRepTimeStamp);
+
+// evalTask.successAttempts ++
+// evalTask.nextRepTimeStamp = spacedR.calculateNextRepetitionTimeStampForEvaluation(evalTask)
+// console.log(`#4:`+ evalTask.nextRepTimeStamp);
+
+
 
 // evalTask.recentResult = false
 // evalTask.failedAttempts++
-// evalTask = spacedR.calculateNextRepetitionTimeStampForEvaluation(evalTask)
-// evalLog.push(evalTask)
-// console.log(evalTask);
+// evalTask.nextRepTimeStamp = spacedR.calculateNextRepetitionTimeStampForEvaluation(evalTask)
+// console.log("#5 false "+evalTask.nextRepTimeStamp);
 
 // evalTask.successAttempts ++
 // evalTask.recentResult = true
 // evalTask = spacedR.calculateNextRepetitionTimeStampForEvaluation(evalTask)
 // console.log(evalTask);
-
+console.log(spacedR.selectEvaluationsForRepetition(evalLog))
+console.log("disse ligger alle i evalLog")
+console.log(evalLog);
 
