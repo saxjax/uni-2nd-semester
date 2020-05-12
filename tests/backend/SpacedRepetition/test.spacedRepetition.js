20007deff14611877652d408daacf4e69ea303c8
Rallefar1103 <rhenri18@student.aau.dk>
2020-05-12 13:33:44 +0200

    oprette test

--- tests/backend/SpacedRepetition/test.spacedRepetition.js
+++ tests/backend/SpacedRepetition/test.spacedRepetition.js
@@ -1,11 +1,25 @@
+/* eslint no-console: off */
+const tape = require(`tape`);
+const testDecorater = require(`tape-promise`).default;
+
+const test = testDecorater(tape);
+const { SpacedRepetition } = require(`../../../node/Models/ `);
+
+let request = { body: {} };
+let actual = true;
+let expected = true;
+let object = new Navn(request);
+
 /* Test af Funktioner */
-// const { SpacedRepetition } = require(`./node/Models/spacedRepetition.js  `);
 
-// test(`Kopier det nedenunder ind i Start Templaten`, async (assert) => {
-//   const SP = new SpacedRepetition();
-//   expected = `forventet output`;
-//   actual = Navn(`vars`);
 
-//   assert.equal(actual, expected,
-//     `{Forventet: ${expected} Reel: ${actual}} Metoden skal kunne X`);
-// });
+test(`Kopier det nedenunder ind i Start Templaten`, async (assert) => {
+  const SP = new SpacedRepetition();
+  actual = SP.getTasksForRepetition();
+  console.log(actual);
+  expected = `forventet output`;
+  actual = Navn(`vars`);
+
+  assert.equal(actual, expected,
+    `{Forventet: ${expected} Reel: ${actual}} Metoden skal kunne X`);
+});
