37ae64ed618273fa74e5f8169059576727ec25b7
MartinBruun <mbm93fen@gmail.com>
2020-05-14 10:21:20 +0200

    fixme fjernet i main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const settings = {
   name: `Server`,   // Angiver funktionaliteten af disse settings. Har ingen funktionsmæssig betydning.
   root: __dirname,  // Medsender root directoriet så MasterController altid henviser til main.js root
   port: 3000,       // Angiver porten som programmet skal kører på.
-  debug: true,      // Angiver om programmet skal køre i udviklingsmode eller i productionmode (FIXME: ikke endnu fuldt implementeret)
+  debug: true,      // Angiver om programmet skal køre i udviklingsmode eller i productionmode.
   skipAccess: true, // Angiver om programmet automatisk skal logges ind som Test User i Tester Group
 };
 const Start = new MasterController(settings);
