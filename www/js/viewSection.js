429ce39ce4fcce9a915f4142cdbd2f4bf44db659
Dreadnogh <mike_johansen101@yahoo.com>
2020-05-21 09:45:13 +0200

    Slet knap virker inde i afsnit.

---
+++ www/js/viewSection.js
@@ -0,0 +1,19 @@
+/* eslint no-unused-vars: 0 */
+/* eslint no-undef: 0 */
+
+async function  deleteSection(sectionID, documentID) {
+  const choice = confirm(`Er du sikker på at du vil slette dette afsnit? \nDenne handling vil også slette tilknyttede evalueringer.`); // eslint-disable-line no-restricted-globals
+  if (choice === true) {
+    const response = await fetch(`/delete/evaluation/section/${sectionID}`, {
+      method: `DELETE`,
+      headers: { "Content-Type": `application/json` },
+    });
+    const responseJSON = await response.json();
+    if (responseJSON.error) {
+      alert(responseJSON.error);
+    }
+    else {
+      window.location.replace(`/view/sectionsAndEvaluations/document/${documentID}`);
+    }
+  }
+}
