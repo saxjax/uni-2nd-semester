cc8c4a8ccec00ecd8f9863a808ed85a651ad6557
MartinBruun <mbm93fen@gmail.com>
2020-05-21 15:24:47 +0200

    hotfix til at test køre nu

--- tests/backend/Database/test.Database.js
+++ tests/backend/Database/test.Database.js
@@ -67,7 +67,6 @@ async function resetDB() {
 test(`Test af Database Klassen i node/Database`, async (assert) => {
   assert.equal(actual, expected, `Skulle gerne være oprettet.`);
 
-  object = new Database();
   object.debug = false; // sættes til false, da vi i tests ikke er interesseret i run-time fejlbeskeder
   /* 1.1 */
   console.log(`1: Databasen skal have adgang til MySQL databasen.`);
