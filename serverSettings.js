6f5caa878e450890157b2d692c3f3041c8c07084
Lasse Skaalum <lskaal19@student.aau.dk>
2020-10-11 09:24:02 +0200

    changed port to 3002

--- serverSettings.js
+++ serverSettings.js
@@ -2,7 +2,7 @@ const settings = {
   // Express settings
   name: `Server`,    // Angiver funktionaliteten af disse settings. Har ingen funktionsmæssig betydning.
   root: __dirname,   // Medsender root directoriet så MasterController altid henviser til main.js root
-  port: 3000,        // Angiver porten som programmet skal kører på.
+  port: 3002,        // Angiver porten som programmet skal kører på.
 
   // developer settings
   debug: true,       // Angiver om programmet skal køre i udviklingsmode eller i productionmode.
